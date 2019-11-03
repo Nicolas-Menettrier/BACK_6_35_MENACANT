f1a88179c22cac1f90849ba5f067f88c703862d6
Arnaud Pinta <arnaud.pinta@epitech.eu>
2019-11-03 09:38:14 +0100

    [NEW] pwd

--- database/database.js
+++ database/database.js
@@ -3,12 +3,10 @@ const uri = process.env.MONGODB_URI || "mongodb+srv://admin:ixEFq7R4sq4pJzM@635m
 
 const database = null;
 
-const connectToDatabase = async function() {
+module.exports = async function() {
     if (database) return database;
-    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = client.db("Poster");
     database = db;
     return db;
-}
-
-module.exports = connectToDatabase;
+};
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.post('/api/authentication', async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).json({ message: 'body values are incorrect' });
-    const user = await database.collection("users").findOne({ email, "password": SHA256(password)})
+    const user = await database().collection("users").findOne({ email, "password": SHA256(password)})
     if (!user) return res.status(401).json({ message: 'wrong email or password' });
     const token = jwt.sign(user, secret, { expiresIn: '3600s' });
     return res.json({ token });
@@ -28,7 +28,7 @@ app.post('/api/authentication', async (req, res) => {
 app.post('/api/registration', async (req, res) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password) return res.status(400).json({ message: 'body values are incorrect' });
-    if (await database.collection("users").findOne({ $or: [{email}, {username}] }))
+    if (await database().collection("users").findOne({ $or: [{email}, {username}] }))
         return res.status(401).json({ message: 'username or email already in use' });
     const user = {
         'id': database.users[database.users.length - 1].id + 1,
